88d8b0960390d44deadb57d3e2d4c193d098dad9
Diego Rodrigues <diegokod@gmail.com>
2022-04-11 21:53:24 -0300

    Componentizando indicadores

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -9,26 +9,17 @@
     </div>
 
     <div class="row mt-5">
-      <div class="col-4">
-        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
-          <p>Vagas abertas</p>
-          <h2>25</h2>
+        <div class="col-4">
+            <indicador-site></indicador-site> 
         </div>
-      </div>
+        
+         <div class="col-4">
+            <indicador-site></indicador-site> 
+        </div>
 
-      <div class="col-4">
-        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
-          <p>Profissionais cadastrados</p>
-          <h2>125</h2>
+         <div class="col-4">
+            <indicador-site></indicador-site> 
         </div>
-      </div>
-
-      <div class="col-4">
-        <div class="h-100 p-5 rounded-3 border bg-light text-dark">
-          <p>Visitantes online</p>
-          <h2>15</h2>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -36,11 +27,13 @@
 <script>
 
 import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
+import IndicadorSite from '@/components/comuns/IndicadorSite.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    PesquisarVaga
+    PesquisarVaga,
+    IndicadorSite
   } 
 }
 </script>
