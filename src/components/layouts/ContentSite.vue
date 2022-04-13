bd634081f9d3f4a3913acf7006b38ed54404f021
Diego Rodrigues <diegokod@gmail.com>
2022-04-12 22:04:09 -0300

    Componente publicar vaga criado

--- src/components/layouts/ContentSite.vue
+++ src/components/layouts/ContentSite.vue
@@ -1,21 +1,41 @@
 <template>
   <div>
-    <HomeView />
-    <PublicarVagas />
-  </div>
-</template>
+     <div>
+    <h1>{{titulo}}</h1>
+    <button @click="atualizarComponente()">Atualizar</button>
+    <button @click="conteudo = 'HomeView'">Home</button>
+    <button @click="conteudo = 'publicar-vaga'">Publicar Vaga</button>
+    <!-- renderizar de modo dinâmico os componentes home e publicar-vaga -->
+    <!--<home></home>-->
+    <!--<publicar-vaga></publicar-vaga>-->
+    <keep-alive>
+      <component :is="conteudo" />
+    </keep-alive>
+     </div>
+  </div> 
+ </template>
 
 <script>
 import HomeView from '../views/HomeView.vue'
-import PublicarVagas from '../views/PublicarVagas.vue'
+import PublicarVaga from '../views/PublicarVaga.vue'
 
  export default {
     name: 'ContentSite',
     components: {
       HomeView,
-      PublicarVagas
+      PublicarVaga
+    },
+    data: () => ({
+    teste: 'O componente foi criado',
+    titulo: 'Componente Conteudo',
+    conteudo: 'HomeView'
+  }),
+     methods: {
+        atualizarComponente() {
+          this.titulo += '*'
     }
-  }
+  },
+ } 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
